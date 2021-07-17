b271d420aa2fdc06bd687185dcbc4bdac2472343
Beshenka <mambatamba6@gmail.com>
2021-07-17 22:07:59 +0900

    nvim

--- hooks.py
+++ hooks.py
@@ -1,2 +1,4 @@
 hooks = {
+        '.config/nvim': 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs\
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
 }
