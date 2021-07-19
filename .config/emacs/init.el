8d7ff8874eba8da7e222f921c1aca6deb6eec93c
Beshenka <mambatamba6@gmail.com>
2021-07-19 21:49:02 +0900

    lsp-mode -> eglot

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -16,33 +16,34 @@
 ;; ========= Common Settings =========
 (setq make-backup-files nil)
 
-;; ==== Tabs Config ====
-;; Create a variable for our preferred tab width
-(setq custom-tab-width 4)
+;; Comments
+(use-package evil-commentary)
+(evil-commentary-mode)
 
-;; Two callable functions for enabling/disabling tabs in Emacs
-(defun disable-tabs () (setq indent-tabs-mode nil))
-(defun enable-tabs  ()
-  (local-set-key (kbd "TAB") 'tab-to-tab-stop)
-  (setq indent-tabs-mode t)
-  (setq tab-width custom-tab-width))
+;; Tabs
+(setq-default tab-width 4)
+(setq-default evil-shift-width tab-width)
+(setq-default indent-tabs-mode nil)
 
-;; Hooks to Enable Tabs
-(add-hook 'prog-mode-hook 'enable-tabs)
-;; Hooks to Disable Tabs
-(add-hook 'lisp-mode-hook 'disable-tabs)
-(add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+;; Neotree
+(use-package neotree
+  :ensure t
+  :bind ("<f8>" . 'neotree-toggle)
+  :init
+  ;; slow rendering
+  (setq inhibit-compacting-font-caches t)
 
-;; Making electric-indent behave sanely
-(setq-default electric-indent-inhibit t)
+  ;; set icons theme
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
-;; Make the backspace properly erase the tab instead of
-;; removing 1 space at a time.
-(setq backward-delete-char-untabify-method 'hungry)
+  ;; Every time when the neotree window is opened, let it find current file and jump to node
+  (setq neo-smart-open t)
 
-;; (OPTIONAL) Shift width for evil-mode users
-;; For the vim-like motions of ">>" and "<<".
-(setq-default evil-shift-width custom-tab-width)
+  ;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically
+  ; (setq projectile-switch-project-action 'neotree-projectile-action)
+
+  ;; show hidden files
+  (setq-default neo-show-hidden-files t))
 
 ;; ========= Ui configuration ==========
 (setq inhibit-startup-message t)
@@ -59,183 +60,124 @@
 (setq display-line-numbers-type 'relative)
 
 (setq redisplay-dont-pause t
-  scroll-margin 1
-  scroll-step 1
-  scroll-conservatively 10000
-  scroll-preserve-screen-position 1)
+      scroll-margin 1
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)
 
 (use-package rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
+;; Appearance
+(use-package doom-themes
+	     :init (load-theme 'doom-one t))
+
+(use-package doom-modeline
+	     :init (doom-modeline-mode 1)
+	     :custom ((doom-modeline-height 12)))
+
+(use-package all-the-icons)
 
 (use-package emojify
-  :hook (after-init . global-emojify-mode))
+	     :hook (after-init . global-emojify-mode))
 
 ;; ========= Keybindings =========
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(use-package general
-  :config
-  (general-create-definer rune/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (rune/leader-keys
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")))
-
-(use-package undo-tree)
-(add-hook 'evil-local-mode-hook 'turn-on-undo-tree-mode)
-
-(use-package evil
+(use-package undo-tree
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  (setq evil-undo-system 'undo-tree)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (global-undo-tree-mode 1))
 
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+(setq evil-want-keybinding nil)
+(use-package evil
+	     :init
+	     (setq evil-want-integration t)
+	     (setq evil-want-C-u-scroll t)
+	     (setq evil-want-C-i-jump nil)
+	     (setq evil-undo-system 'undo-tree)
+	     :config
+	     (evil-mode 1)
+	     (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+	     (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
+	     ;; Use visual line motions even outside of visual-line-mode buffers
+	     (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+	     (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-(evil-ex-define-cmd "q" 'kill-this-buffer)
+	     (evil-set-initial-state 'messages-buffer-mode 'normal)
+	     (evil-set-initial-state 'dashboard-mode 'normal))
+
+(evil-ex-define-cmd "q" 'kill-buffer-and-window)
 (use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
-;; ======== Color theme =========
-(use-package doom-themes
-	     :init (load-theme 'doom-one-light t))
-
-(use-package all-the-icons)
-
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 12)))
+	     :after evil
+	     :config
+	     (evil-collection-init))
 
 ;; ========= Packages =========
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 1))
-
 (use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+	     :diminish
+	     :bind (("C-s" . swiper)
+		    :map ivy-minibuffer-map
+		    ("TAB" . ivy-alt-done)
+		    ("C-l" . ivy-alt-done)
+		    ("C-j" . ivy-next-line)
+		    ("C-k" . ivy-previous-line)
+		    :map ivy-switch-buffer-map
+		    ("C-k" . ivy-previous-line)
+		    ("C-l" . ivy-done)
+		    ("C-d" . ivy-switch-buffer-kill)
+		    :map ivy-reverse-i-search-map
+		    ("C-k" . ivy-previous-line)
+		    ("C-d" . ivy-reverse-i-search-kill))
+	     :config
+	     (ivy-mode 1))
 
 (use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
+	     :init
+	     (ivy-rich-mode 1))
 
 (use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (counsel-mode 1))
+	     :bind (("C-M-j" . 'counsel-switch-buffer)
+		    :map minibuffer-local-map
+		    ("C-r" . 'counsel-minibuffer-history))
+	     :config
+	     (counsel-mode 1))
 
-;; ========= LSP Mode =========
-(defun efs/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode))
-
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :hook (lsp-mode . efs/lsp-mode-setup)
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :config
-  (lsp-enable-which-key-integration t))
-
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom
-  (lsp-ui-doc-enable t)
-  (lsp-ui-peek-enable t)
-  (lsp-ui-doc-show-with-cursor t))
-
-(use-package lsp-ivy)
-(use-package flycheck)
-
-;; Company (Completion)
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0))
+;; ======== Code =======
+;; Complete
+(use-package company)
 
 (use-package company-box
-  :hook (company-mode . company-box-mode))
+	     :hook (company-mode . company-box-mode))
 
-;; ==== Gopls ====
-(require 'lsp-mode)
-(add-hook 'go-mode-hook #'lsp-deferred)
+;; Flymake
+(use-package flymake-diagnostic-at-point
+  :after flymake
+  :config
+  (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode))
 
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+;; Eldoc
+(use-package eldoc-box)
+(add-hook 'eglot--managed-mode-hook #'eldoc-box-hover-mode t)
 
-;; ========= Commenting =========
-(use-package evil-nerd-commenter)
-(use-package evil-leader)
+;; LSP
+(use-package eglot)
 
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-key
-  "ci" 'evilnc-comment-or-uncomment-lines
-  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
-  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
-  "cc" 'evilnc-copy-and-comment-lines
-  "cp" 'evilnc-comment-or-uncomment-paragraphs
-  "cr" 'comment-or-uncomment-region
-  "cv" 'evilnc-toggle-invert-comment-line-by-line
-  "."  'evilnc-copy-and-comment-operator
-  "\\" 'evilnc-comment-operator ; if you prefer backslash key
-  )
+;; Golang
+(use-package go-mode)
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-hook 'go-mode-hook 'company-mode)
 
-;; ========== Treemacs =========
-(use-package treemacs
-             :bind
-             (:map global-map
-                   ([f8] . treemacs)
-                   ([f9] . lsp-treemacs-symbols)))
-(use-package treemacs-evil)
+(defun eglot-format-buffer-on-save ()
+  (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
+(add-hook 'go-mode-hook #'eglot-format-buffer-on-save)
 
-(use-package lsp-treemacs
-  :after lsp)
+(setq-default eglot-workspace-configuration
+    '((:gopls .
+        ((staticcheck . t)))))
 
-(defun ml/bash ()
+;; ======== Functions ========
+(defun bs/bash ()
   "Start a terminal emulator in a new window."
   (interactive)
   (split-window-vertically)
@@ -243,9 +185,7 @@
   (other-window 1)
   (ansi-term (executable-find "bash")))
 
-;; ==============
-;; M  A  G  I  C
-;; ==============
+;; ==========================
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
